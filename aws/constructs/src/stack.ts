9669ddedbdc9de10a0bece2050077e16cb9ff5a3
aereal <aereal@aereal.org>
2018-10-24 21:42:00 +0900

    Implement essential handler

--- aws/constructs/src/stack.ts
+++ aws/constructs/src/stack.ts
@@ -1,7 +1,21 @@
+import {
+  Code,
+  Function as LambdaFunction,
+  Runtime,
+  Tracing,
+} from "@aws-cdk/aws-lambda";
 import { App, Stack, StackProps } from "@aws-cdk/cdk";
 
 export class KanjosenStack extends Stack {
+  private lambda: LambdaFunction;
+
   constructor(parent: App, id: string, props?: StackProps) {
     super(parent, id, props);
+    this.lambda = new LambdaFunction(this, "KanjosenClickHandler", {
+      code: Code.asset("./lambda/dist"),
+      handler: "index.KanjosenClickHandler",
+      runtime: Runtime.NodeJS810,
+      tracing: Tracing.Active,
+    });
   }
 }
---
+++ lambda/dist/index.js
@@ -0,0 +1,4 @@
+exports.KanjosenClickHandler = async (event, context) => {
+  console.log(`event: ${event}; context: ${context}`);
+  return 'kanjo';
+};
