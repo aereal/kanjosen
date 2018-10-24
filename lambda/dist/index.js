cbdc840ade3067e0eb3a975a115f26f7f190eea8
aereal <aereal@aereal.org>
2018-10-24 22:02:14 +0900

    Show feelings

--- lambda/dist/index.js
+++ lambda/dist/index.js
@@ -1,4 +1,15 @@
+const { inspect } = require('util');
 exports.KanjosenClickHandler = async (event, context) => {
-  console.log(`event: ${event}; context: ${context}`);
+  console.log(`event: ${inspect(event)}; context: ${inspect(context)}`);
+  const { deviceEvent } = event;
+  console.log(`deviceEvent: ${inspect(deviceEvent)}`);
+  if ('buttonClicked' in deviceEvent) {
+    const { buttonClicked } = deviceEvent;
+    if (buttonClicked.clickType === 'DOUBLE') {
+      console.log('Feeling: Negative :(');
+    } else {
+      console.log('Feeling: Positive :)');
+    }
+  }
   return 'kanjo';
 };
